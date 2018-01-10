000c24e968e42c747d1425af53b3b6b9ef73c933
Knut Valen <knut.valen@gmail.com>
2018-01-10 15:53:17 +0100

    remove styling

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ class App extends React.Component {
     render() {
         return (
             <Provider store={store}>
-                <div className="App">
+                <div>
                     <TimeTracking />
                 </div>
             </Provider>
